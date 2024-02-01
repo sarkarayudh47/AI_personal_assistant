3882569a54834f4679616d2c4fec970ce927641d
Shubhabrata Mondal <139055156+mondalshubh20@users.noreply.github.com>
2024-02-01 20:09:19 +0530

    Create total_time_sum.py

---
+++ total_time_sum.py
@@ -0,0 +1,12 @@
+import mysql.connector as myconn
+mydb = myconn.connect(
+    host="bzww6voywepttascpl44-mysql.services.clever-cloud.com",
+    user="uf9srr9rwtttokwi",
+    password="SLk5emfZrpnqSMwspuyU",
+    database="bzww6voywepttascpl44")
+
+
+db_cursor = mydb.cursor()
+db_cursor.execute("SELECT SUM(Total Timing) AS total_sum FROM subdata;")
+for i in db_cursor:
+    print(i)
